8aa007fc1d60f6d7397a54ce21b717522d788656
Muse775 <gaumus.work@gmail.com>
2020-08-07 16:37:38 +0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $('.slider__inner').slick({
+
+        nextArrow:'<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow:'<button type="button" class="slick-btn slick-prev"></button>',
+        dots: true,
+        arrows:false
+        
+    });
+
+});
